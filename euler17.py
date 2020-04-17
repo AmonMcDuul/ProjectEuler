d35e8e7fea8e3d5db126ce58ac4a4fe4add3e841
AmonMcDuul <joranomic@hotmail.com>
2020-04-17 13:03:14 +0200

    17

--- euler17.py
+++ euler17.py
@@ -3,13 +3,13 @@
 #NOTE: Do not count spaces or hyphens. For example, 342 (three hundred and forty-two) contains 23 letters and 115 (one hundred and fifteen) contains 20 letters. The use of "and" when writing out numbers is in compliance with British usage.
 
 def numberToWord(n):
-    numbers = { 0 : 'zero', 1 : 'one', 2 : 'two', 3 : 'three', 4 : 'four', 5 : 'five', \
-          6 : 'six', 7 : 'seven', 8 : 'eight', 9 : 'nine', 10 : 'ten', \
-          11 : 'eleven', 12 : 'twelve', 13 : 'thirteen', 14 : 'fourteen', \
-          15 : 'fifteen', 16 : 'sixteen', 17 : 'seventeen', 18 : 'eighteen', \
-          19 : 'nineteen', 20 : 'twenty', \
-          30 : 'thirty', 40 : 'forty', 50 : 'fifty', 60 : 'sixty', \
-          70 : 'seventy', 80 : 'eighty', 90 : 'ninety' }
+    numbers = { 0 : "zero", 1 : "one", 2 : "two", 3 : "three", 4 : "four", 5 : "five", \
+          6 : "six", 7 : "seven", 8 : "eight", 9 : "nine", 10 : "ten", \
+          11 : "eleven", 12 : "twelve", 13 : "thirteen", 14 : "fourteen", \
+          15 : "fifteen", 16 : "sixteen", 17 : "seventeen", 18 : "eighteen", \
+          19 : "nineteen", 20 : "twenty", \
+          30 : "thirty", 40 : "forty", 50 : "fifty", 60 : "sixty", \
+          70 : "seventy", 80 : "eighty", 90 : "ninety" }
     k = 1000
     m = k * 1000
 
@@ -19,17 +19,17 @@ def numberToWord(n):
         if n % 10 == 0:
             return numbers[n]
         else:
-            return numbers[n // 10 * 10] + ' ' + numbers[n % 10]
+            return numbers[n // 10 * 10] + " " + numbers[n % 10]
     if (n < k):
         if n % 100 == 0:
-            return numbers[n // 100] + ' hundred'
+            return numbers[n // 100] + " hundred"
         else:
-            return numbers[n // 100] + ' hundred and ' + numberToWord(n % 100)
+            return numbers[n // 100] + " hundred and " + numberToWord(n % 100)
     if (n < m):
         if n % k == 0:
-            return numberToWord(n // k) + ' thousand'
+            return numberToWord(n // k) + " thousand"
         else:
-            return numberToWord(n // k) + ' thousand and ' + numberToWord(n % k)
+            return numberToWord(n // k) + " thousand and " + numberToWord(n % k)
 
 results = ""
 for i in range(1,1001):
