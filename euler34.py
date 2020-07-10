59a343be2a32646d764d17de67c6b2581790e50c
AmonMcDuul <joranomic@hotmail.com>
2020-07-10 12:52:31 +0200

    34

--- euler34.py
+++ euler34.py
@@ -2,3 +2,16 @@
 # Find the sum of all numbers which are equal to the sum of the factorial of their digits.
 # Note: as 1! = 1 and 2! = 2 are not sums they are not included.
 
+from math import factorial
+
+number = 0
+
+for i in range(3,100000):
+    s = str(i)
+    result = 0
+    for item in s:
+        result += factorial(int(item))
+        if result == int(s):
+            number += result
+    
+print("Result is: ", number)
