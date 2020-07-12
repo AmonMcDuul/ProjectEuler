e0897f14251311cce2730f3f722ef5ba2ba9481b
AmonMcDuul <joranomic@hotmail.com>
2020-07-12 21:22:51 +0200

    42

--- test2.py
+++ test2.py
@@ -1,16 +1,3 @@
-numbersResult = []
-
-for items in range(1000,9999):
-    digits = [int(x) for x in str(items)]
-    a=0
-    b=0
-    count=0
-    for i in digits:
-        a = i**4
-        b = b + a
-        count = count + 1
-        if count == 4 and b == items:
-            print("Succes! ", b, "    Digits: ", items)
-            numbersResult.append(b)
-
-print(sum(numbersResult))
+n = 5
+r = n(n+1)/2
+print(r)
