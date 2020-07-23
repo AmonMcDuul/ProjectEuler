32cb79109bc0f21f54ea313df7a1f2e050c274db
AmonMcDuul <joranomic@hotmail.com>
2020-07-23 16:21:00 +0200

    ..

--- euler54.py
+++ euler54.py
@@ -67,24 +67,33 @@ OnePair = []
 #     High Card: Highest value card.
 HighCard = []
 
-D= ['1D','2D','3D','4D',"5D",'6D','7D','8D','9D','JD','QD','KD','AD']
+D = ['1D','2D','3D','4D',"5D",'6D','7D','8D','9D','JD','QD','KD','AD']
 S = ['1S','2S','3S','4S',"S5",'6S','7S','8S','9S','JS','QS','KS','AS']
 C = ['1C','2C','3C','4C',"C5",'6C','7C','8C','9C','JC','QC','KC','AC']
 H = ['1H','2H','3H','4H',"5H",'6H','7H','8H','9H','JH','QH','KH','AH']
 
+
+def checkCard(one,two):
+    royalFlush(one,two)
+    return
+
+def royalFlush(one,two):
+    f = ['A','K','Q','J','T']
+    if all(x in one for x in f):
+        print(one)
+    if all(x in two for x in f):
+        print(two, 'two')
+    return
+
 cards = []
 for item in open("euler54poker.txt", "r"):
     cards.append([item.strip()]) 
-print(cards)
-
+    
 for item in cards:
     count = 0
-    p1 = []
-    p2 = []
+    one = ''
+    two = ''
     for x in item:
-        p1.append(x[:14])
-        p2.append(x[15:])
-    print(p1,  p2)
-    for i in range(1,13):
-        if C[1] in str(x):
-            print("farts", x) 
+        one = (x[:14])
+        two = (x[15:])
+        checkCard(one,two)
