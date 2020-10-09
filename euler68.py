9a0f3cd1b8a5cfd3cfabfd5223c656de0b37c994
AmonMcDuul <joranomic@hotmail.com>
2020-10-09 15:15:28 +0200

    70

--- euler68.py
+++ euler68.py
@@ -13,11 +13,11 @@
 # By concatenating each group it is possible to form 9-digit strings; the maximum string for a 3-gon ring is 432621513.
 # Using the numbers 1 to 10, and depending on arrangements, it is possible to form 16- and 17-digit strings. What is the maximum 16-digit string for a "magic" 5-gon ring?
 
-Met de hand gedaan.
-getallen 1 t/m 5 in de binnencirkel
-6 t/m 10 in de buitencirkel.
+# Met de hand gedaan.
+# getallen 1 t/m 5 in de binnencirkel
+# 6 t/m 10 in de buitencirkel.
 
-Opgeteld is het 14.
+# Opgeteld is het 14.
 
 #      6
 #              10
--- euler70.py
+++ euler70.py
@@ -3,3 +3,27 @@
 # Interestingly, φ(87109)=79180, and it can be seen that 87109 is a permutation of 79180.
 # Find the value of n, 1 < n < 107, for which φ(n) is a permutation of n and the ratio n/φ(n) produces a minimum.
 
+# 10000000
+
+def phi(n):    
+    result = n
+    p = 2
+    while(p * p <= n):
+        if (n % p == 0):              
+            while (n % p == 0):
+                n = int(n / p)
+            result -= int(result / p)
+        p += 1
+    if (n > 1):
+        result -= int(result / n)
+    return result
+
+result = 100
+
+for i in range(2, 10000000):
+    x = phi(i)
+    if sorted(str(i)) == sorted(str(x)): 
+        x = i/x 
+        if x < result:
+            result = x
+            print("lowest: ", result, " from number: ", i)
