17c2708b0d64303068912bc73c8c7b4dc48c4447
AmonMcDuul <joranomic@hotmail.com>
2020-10-27 16:30:51 +0100

    80

--- euler80.py
+++ euler80.py
@@ -3,3 +3,22 @@
 # The square root of two is 1.41421356237309504880..., and the digital sum of the first one hundred decimal digits is 475.
 # For the first one hundred natural numbers, find the total of the digital sums of the first one hundred decimal digits for all the irrational square roots.
 
+import math
+from decimal import *
+
+getcontext().prec = 102
+
+result = 0
+
+for i in range(1,101):
+    x = Decimal(i).sqrt()
+    if x % int(x) != 0:
+        p = 0
+        c = 0
+        for i in str(x):
+            if i != '.':
+                c += 1
+                result += int(i)
+                if c == 100:
+                    break
+print(result)
